fc65929347d47585085dd49bfeb62d88db65ed67
Alex Katorzhin <46688550+katorzhin@users.noreply.github.com>
2019-01-31 16:22:13 +0200

    delete tree.java

--- Tree.java
+++
@@ -1,8 +0,0 @@
-public class Tree {
-    public static void main(String[] args) {
-        System.out.println("\t\t  /\\");
-        System.out.println("\t\t /  \\"+"\n\t\t/ || \\"+"\n\t   /  ||  \\"+"\n\t  /   ||   \\"+"\n\t /    ||\t\\");
-        System.out.println("\t/\t  ||\t \\"+"\n\t\t  ||");
-        System.out.println("   ----------------");
-    }
-}
