d8d62aa1c6062ef03528907edd75844ff7cabf2f
katorzhin <alex.katorzhin@gmail.com>
2019-02-11 15:55:06 +0200

    SimpleDotCom

---
+++ GameLauncher.java
@@ -0,0 +1,8 @@
+package HeadFirst;
+
+public class GameLauncher {
+    public static void main(String[] args) {
+        GuessGame game=new GuessGame();
+        game.startGame();
+    }
+}
---
+++ Player.java
@@ -0,0 +1,11 @@
+package HeadFirst;
+
+public class Player {
+    int number = 0;
+
+    public void guess() {
+        number = (int) (Math.random() * 10);
+        System.out.println("Думаю,это число " + number);
+
+    }
+}
