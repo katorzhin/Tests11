8d30ad3632e05736abf7a59d102b57e09439b5df
Alex <alex.katorzhin@gmail.ru>
2019-04-04 12:00:06 +0300

    gitignore

---
+++ Tasks/Task62.java
@@ -0,0 +1,25 @@
+package Tasks;
+
+public class Task62 {
+    public static void main(String[] args) {
+        System.out.println(getZeroCount(1, 3, 5, 5, 7, 0, 0, 9, 9, 3, 5, 0, 0, 0, 0, 5));
+    }
+
+    public static int getZeroCount(int... numbers) {
+        int tempZeroCount = 0;
+        int countMax = 0;
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i] == 0) {
+                tempZeroCount++;
+            } else {
+                countMax = (countMax > tempZeroCount) ? countMax : tempZeroCount;
+                tempZeroCount = 0;
+            }
+            if (numbers[i]==0&&numbers.length-1==i){
+                countMax = (countMax > tempZeroCount) ? countMax : tempZeroCount;
+                tempZeroCount = 0;
+            }
+        }
+        return countMax;
+    }
+}
